4e8d98f09d2e8cb86f435df39cea0b6aee5c8bc2
Saikrishna9110 <90841277+Saikrishna9110@users.noreply.github.com>
2025-10-18 22:15:28 +0530

    Update ResumeService.js

--- src/api/ResumeService.js
+++ src/api/ResumeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURLL = "http://localhost:8080";
+export const baseURLL = "ai-backend-production-7eb5.up.railway.app";
 
 export const axiosInstance = axios.create({
   baseURL: baseURLL,
